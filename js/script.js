799ca7039ca2a25e239ddd830c078203ccd6d026
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-15 16:10:48 +0200

    add snack 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 //TODO 1. Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //TODO Stampare a schermo la bici con peso minore.
 
+var parcoBici = [
+    {
+        nome : 'ktm',
+        peso : 10
+    },
+    {
+        nome : 'torpado',
+        peso : 15
+    },
+    {
+        nome : 'bianchi',
+        peso : 6
+    }
+];
+
+//* Controllo qual'è la più leggera
+var pesoMin = parcoBici[0];
+
+for(var i = 1; i < parcoBici.length; i++){
+    if(parcoBici[i].peso < pesoMin.peso){
+        pesoMin = parcoBici[i];
+    }
+}
+
+//* Stampo a schermo la più leggera
+console.log(pesoMin);
+
 
 
 
