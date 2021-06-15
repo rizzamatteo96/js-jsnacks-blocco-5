cd10c02066111a498a7d08756c317da4dc33fc1b
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-15 16:26:30 +0200

    Closing project add snack 3 fix some comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//TODO 1. Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//TODO SNACK 1. Creare un array di oggetti. Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //TODO Stampare a schermo la bici con peso minore.
 
 var parcoBici = [
@@ -32,7 +32,7 @@ console.log(pesoMin);
 
 
 
-//TODO 2. Crea un array di 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza.
+//TODO SNACK 2. Crea un array di 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza.
 //TODO Calcola quanto pesano tutte le zucchine.
 
 
@@ -101,4 +101,90 @@ console.log('peso totale zucchine = ' + pesoTot + 'gr');
 
 
 
-//TODO 3. Crea un array di 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine
+//TODO SNACK 3. Crea un array di 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine
+
+var zucchineNew = [
+    {
+        varieta : 'a',
+        peso : 100,
+        lunghezza : 10
+    },
+    {
+        varieta : 'b',
+        peso : 250,
+        lunghezza : 18
+    },
+    {
+        varieta : 'c',
+        peso : 300,
+        lunghezza : 20
+    },
+    {
+        varieta : 'd',
+        peso : 180,
+        lunghezza : 15
+    },
+    {
+        varieta : 'e',
+        peso : 140,
+        lunghezza : 12
+    },
+    {
+        varieta : 'f',
+        peso : 400,
+        lunghezza : 25
+    },
+    {
+        varieta : 'g',
+        peso : 500,
+        lunghezza : 30
+    },
+    {
+        varieta : 'h',
+        peso : 380,
+        lunghezza : 22
+    },
+    {
+        varieta : 'i',
+        peso : 130,
+        lunghezza : 11
+    },
+    {
+        varieta : 'l',
+        peso : 600,
+        lunghezza : 35
+    }
+];
+
+var zucchineCorte = [];
+var zucchineLunghe = [];
+
+//* divido le zucchine in due gruppi a sezonda della lunghezza
+for(var i = 0; i < zucchineNew.length; i++){
+    if (zucchineNew[i].lunghezza < 15){
+        zucchineCorte.push(zucchineNew[i]);
+    }
+    else{
+        zucchineLunghe.push(zucchineNew[i]);
+    }
+}
+
+//* calcolo il peso complessivo dei due gruppi e lo mostro a schermo
+console.log('Il gruppo delle zucchine corte pesa in totale ' + calcoloPeso(zucchineCorte) + 'gr');
+console.log('Il gruppo delle zucchine lunghe pesa in totale ' + calcoloPeso(zucchineLunghe) + 'gr');
+
+
+
+
+
+
+
+
+//! SEZIONE PER LE FUNZIONI
+function calcoloPeso(arrayObject){
+    var somma = 0;
+    for(var i = 0; i < arrayObject.length; i++){
+        somma += arrayObject[i].peso;
+    }
+    return somma;
+}
